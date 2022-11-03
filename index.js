5a30add4167889221389c4c5145bd071a32a3b3a
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-03 07:42:27 +0600

    connect mongodb with server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,6 +11,14 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("Ema john server is running");
+});
+
+/* Mongodb Connection */
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.muj8g87.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
 });
 
 app.listen(port, () => {
